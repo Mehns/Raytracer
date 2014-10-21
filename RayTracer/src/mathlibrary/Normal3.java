b56918a265c6d558888858f7849212906e7aa7e0
Isch <Isch@192.168.1.6>
2014-10-21 15:38:39 +0200

    Normal3 implemented

--- RayTracer/src/mathlibrary/Normal3.java
+++ RayTracer/src/mathlibrary/Normal3.java
@@ -10,5 +10,61 @@ package mathlibrary;
  * @author Christian
  */
 public class Normal3 {
+    public final double x;
+    public final double y;
+    public final double z;
+
+    public Normal3(final double x, final double y, final double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    public Normal3 mul(final double n) {
+        return new Normal3(this.x * n, this.y * n, this.z * n);
+    }
+    
+    public Normal3 add(final Normal3 n) {
+        return new Normal3(this.x + n.x, this.y + n.y, this.z + n.z);
+    }
+    
+    public double dot(Vector3 v){
+        return this.x * v.x + this.x * v.x + this.x * v.x;
+    }
+
+    @Override
+    public String toString() {
+        return "Normal3{" + "x=" + x + ", y=" + y + ", z=" + z + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 19 * hash + (int) (Double.doubleToLongBits(this.x) ^ (Double.doubleToLongBits(this.x) >>> 32));
+        hash = 19 * hash + (int) (Double.doubleToLongBits(this.y) ^ (Double.doubleToLongBits(this.y) >>> 32));
+        hash = 19 * hash + (int) (Double.doubleToLongBits(this.z) ^ (Double.doubleToLongBits(this.z) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Normal3 other = (Normal3) obj;
+        if (Double.doubleToLongBits(this.x) != Double.doubleToLongBits(other.x)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.y) != Double.doubleToLongBits(other.y)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.z) != Double.doubleToLongBits(other.z)) {
+            return false;
+        }
+        return true;
+    }
     
 }
