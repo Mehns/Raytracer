bcb0efe26aa7e576e8dc1b34151721d01a69f9cb
Lena Messmer <s55034@beuth-hochschule.de>
2014-11-03 10:55:01 +0100

    updated mat3x3

--- RayTracer/src/mathlibrary/Mat3x3.java
+++ RayTracer/src/mathlibrary/Mat3x3.java
@@ -68,6 +68,88 @@ public class Mat3x3 {
                             this.m31 * p.x + this.m32 * p.y + this.m33 * p.z        
         );
     }
-        
     
+    public Mat3x3 changeCol1 (final Vector3 v){
+        return new Mat3x3(
+                            v.x, this.m12, this.m13,
+                            v.y, this.m22, this.m23,
+                            v.z, this.m32, this.m33
+        );
+    }
+
+    public Mat3x3 changeCol2 (final Vector3 v){
+        return new Mat3x3(
+                            this.m11, v.x, this.m13,
+                            this.m21, v.y, this.m23,
+                            this.m31, v.z, this.m33
+        );
+    }
+    
+    public Mat3x3 changeCol3 (final Vector3 v){
+        return new Mat3x3(
+                            this.m11, this.m12, v.x,
+                            this.m21, this.m22, v.y, 
+                            this.m31, this.m32, v.z 
+        );
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.m11) ^ (Double.doubleToLongBits(this.m11) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.m12) ^ (Double.doubleToLongBits(this.m12) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.m13) ^ (Double.doubleToLongBits(this.m13) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.m21) ^ (Double.doubleToLongBits(this.m21) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.m22) ^ (Double.doubleToLongBits(this.m22) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.m23) ^ (Double.doubleToLongBits(this.m23) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.m31) ^ (Double.doubleToLongBits(this.m31) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.m32) ^ (Double.doubleToLongBits(this.m32) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.m33) ^ (Double.doubleToLongBits(this.m33) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Mat3x3 other = (Mat3x3) obj;
+        if (Double.doubleToLongBits(this.m11) != Double.doubleToLongBits(other.m11)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.m12) != Double.doubleToLongBits(other.m12)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.m13) != Double.doubleToLongBits(other.m13)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.m21) != Double.doubleToLongBits(other.m21)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.m22) != Double.doubleToLongBits(other.m22)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.m23) != Double.doubleToLongBits(other.m23)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.m31) != Double.doubleToLongBits(other.m31)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.m32) != Double.doubleToLongBits(other.m32)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.m33) != Double.doubleToLongBits(other.m33)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Mat3x3{" + "m11=" + m11 + ", m12=" + m12 + ", m13=" + m13 + ", m21=" + m21 + ", m22=" + m22 + ", m23=" + m23 + ", m31=" + m31 + ", m32=" + m32 + ", m33=" + m33 + ", determinant=" + determinant + '}';
+    }
+  
 }
