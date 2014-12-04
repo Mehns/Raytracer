c17f78025488b50ee52fca5abb06a0734d65a53e
Christian Mehns <s55689@beuth-hochschule.de>
2014-12-04 14:19:07 +0100

    SingleColorMaterial finished, commented

--- RayTracer/src/material/SingleColorMaterial.java
+++ RayTracer/src/material/SingleColorMaterial.java
@@ -3,20 +3,29 @@ package material;
 import color.Color;
 import geometry.Hit;
 import world.World;
+
 /**
- *
+ * represents a material with one solid color without any shading
  * @author Christian Mehns <s55689@beuth-hichschule.de>
  */
 public class SingleColorMaterial extends Material{
+
+    /**
+     * Color of Material
+     */
     public Color color;
     
+    /**
+     * Constructor of SingleColorMaterial
+     * @param color Color of material
+     */
     public SingleColorMaterial(Color color){
         this.color = color;
     }
     
     @Override
-    public Color colorFor(Hit hit, World world) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Color colorFor(final Hit hit, final World world) {
+        return color;
     }
     
 }
